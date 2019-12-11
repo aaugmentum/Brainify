28f18bf79b2e34564d788dc55941ed51b40d9ab9
Kamron Saliev <kamron.saliev5@gmail.com>
2019-12-11 21:39:51 +0500

    Added music and sound managers, added tap sound on buttons click

---
+++ Brainify_UnityProject/Assets/_Scripts/Managers/Sound.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class Sound : MonoBehaviour
+{
+    public AudioClip defaultClip;
+    public AudioClip[] clips;
+
+    public void PlayDefaultSound()
+    {
+        SoundsManager.instance.PlaySound(defaultClip);
+    }
+
+    public void PlaySoundClip(int clipIndex = 0)
+    {
+        if (clipIndex >= clips.Length) return;
+        SoundsManager.instance.PlaySound(clips[clipIndex]);
+    }
+}

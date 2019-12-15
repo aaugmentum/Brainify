3b05f0e05bb213d7de38ea78be3de1f7d021d885
idavron <idavron.u@gmail.com>
2019-12-15 13:40:13 +0500

    Fix

---
+++ Brainify_UnityProject/Assets/_Scripts/ButtonsLoaderScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class ButtonsLoaderScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Button[] Buttons;
+    void Start()
+    {
+        int i = 0;
+        foreach(KeyValuePair<string, string> game in IPCManager.instance.gamesMap){
+            Buttons[i].gameObject.GetComponentInChildren<Text>().text = game.Key;
+            i++;
+        }
+        while(i < Buttons.Length){
+            Buttons[i].gameObject.SetActive(false);
+            i++;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
